3c03730c9c5ebff2156a05dd850cf2ecc9573d15
Gabriel Silva <40872543+silvagabriel62@users.noreply.github.com>
2019-01-04 17:58:50 -0200

    Update index.php

--- upload/index.php
+++ upload/index.php
@@ -1,4 +1,16 @@
 <?php
+
+	/*
+
+		pyprotect - by Gabriel Silva
+		Version 0.1alpha
+		http://gabrielsilva.tk/pyprotect
+
+		This is an open source software
+		Read LICENSE file before using it
+
+	*/
+
 	header("Location: http://gabrielsilva.tk/pyprotect");
 	exit();
 ?>
